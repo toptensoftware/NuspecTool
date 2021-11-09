135b78273c0e46fd0df41d3b1b0a0be34f276f94
Brad Robinson <contact@toptensoftware.com>
2021-11-09 11:07:46 +1100

    b0.1.104

--- version.cs
+++ version.cs
@@ -3,13 +3,13 @@
 using System;
 using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2021 Topten Software. All Rights Reserved")]
-[assembly: AssemblyVersion("0.1.103")]
-[assembly: AssemblyFileVersion("0.1.103")]
+[assembly: AssemblyVersion("0.1.104")]
+[assembly: AssemblyFileVersion("0.1.104")]
 [assembly: AssemblyCompany("Topten Software")]
 [assembly: AssemblyProduct("nuspec_tool")]
 
 static class BuildInfo
 {
-	public static DateTime Date = new DateTime(2021, 11, 9, 0, 6, 41, DateTimeKind.Utc);
+	public static DateTime Date = new DateTime(2021, 11, 9, 0, 7, 38, DateTimeKind.Utc);
 }
 	
